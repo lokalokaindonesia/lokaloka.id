25923a90c12380e7cf79a34f9fd5b9d8753d8a8f
Devarista <kampungvector@gmail.com>
2021-06-23 10:05:45 +0700

    update .env

--- next.config.js
+++ next.config.js
@@ -2,12 +2,4 @@ module.exports = {
     images: {
         domains: ['api.lokaloka.id', 'source.unsplash.com', 'res.cloudinary.com'],
     },
-    // env: {
-    //     NEXT_URL: 'http://localhost:3000',
-    //     NEXTAUTH_URL: 'http://localhost:3000',
-    //     NEXT_PUBLIC_API_URL: 'http://localhost:1337',
-    //     NEXT_PUBLIC_DATABASE_URL: 'mongodb://localhost:27017/lokaloka',
-    //     GOOGLE_CLIENT_ID: '321274656766-dk50dcdfsm4e09kch8n2okohg8lgveau.apps.googleusercontent.com',
-    //     GOOGLE_CLIENT_SECRET: 'xQcOxmZNniSxqa5BuwHzOljX',
-    // }
 }
