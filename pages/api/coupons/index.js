a84a7bad9d9395e0ff93cf9563f627ba2ae1413c
Devarista <kampungvector@gmail.com>
2021-07-08 15:27:48 +0700

    coupon

--- pages/api/coupons/index.js
+++ pages/api/coupons/index.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export default async (req, res) => {
+    // get coupon data
     const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/coupons`)
     return res.json(data)
 }
