a6033d1a4216c036d0695c93a10d1977845d6d13
Arza Aldi K <kampungvector@gmail.com>
2021-08-10 21:55:52 +0700

    update api

--- pages/api/expedition/cost/index.js
+++ pages/api/expedition/cost/index.js
@@ -1,20 +1,16 @@
 import axios from "axios"
 
 export default async (req, res) => {
-    try {
-        const { data } = await axios.post(`${process.env.RAJA_ONGKIR_URL}/cost`, {
-            origin: process.env.RAJA_ONGKIR_ORIGIN,
-            destination: req.body.destination,
-            weight: req.body.weight,
-            courier: process.env.RAJA_ONGKIR_COURIER
-        }, {
-            headers: {
-                key: process.env.RAJA_ONGKIR_API_KEY
-            }
-        })
+    const { data } = await axios.post(`${process.env.RAJA_ONGKIR_URL}/cost`, {
+        origin: process.env.RAJA_ONGKIR_ORIGIN,
+        destination: req.body.destination,
+        weight: req.body.weight,
+        courier: process.env.RAJA_ONGKIR_COURIER
+    }, {
+        headers: {
+            key: process.env.RAJA_ONGKIR_API_KEY
+        }
+    })
 
-        return res.status(200).json(data.rajaongkir.results[0].costs[1].cost[0])
-    } catch ({ message }) {
-        return res.status(400).json(message)
-    }
+    return res.json(data.rajaongkir.results[0].costs[1].cost[0])
 }
