103caf2cb10f4081d938a239cdbf0673f3abf31e
Arza Aldi K <kampungvector@gmail.com>
2021-09-09 17:56:40 +0700

    update invoice callback

--- pages/api/wh/payment/invoice/index.js
+++ pages/api/wh/payment/invoice/index.js
@@ -1,22 +1,19 @@
 import axios from "axios"
 
 export default async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405).json("Method not Allowed")
+    }
+
     try {
         const getTransaction = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/transactions?code=${req.body.external_id}`)
 
         const transaction = await getTransaction.data[0]
-        if (!transaction) {
-            return res.status(404)
-        }
 
         const updateInvoiceTransaction = await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/transactions/${transaction.id}`,
             { paymentStatus: req.body.status }
         )
         const data = await updateInvoiceTransaction.data
-
-        if (!data) {
-            return res.status(404).json('data not found')
-        }
 
         return res.json(data)
     } catch (error) {
