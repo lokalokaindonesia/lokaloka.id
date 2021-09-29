ec1537bc43a25109d19ffc1809859ea1ed6c78d0
Arza Aldi K <kampungvector@gmail.com>
2021-09-29 18:43:17 +0700

    fix bugs

--- pages/api/product-categories/[slug].js
+++ pages/api/product-categories/[slug].js
@@ -1,6 +1,10 @@
 import axios from "axios"
 
 export default async (req, res) => {
-    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/product-categories?slug_eq=${req.query.slug}`)
-    return res.json(data[0])
+    try {
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/product-categories?slug_eq=${req.query.slug}`)
+        return res.json(data[0])
+    } catch (error) {
+        return res.json(error)
+    }
 }
--- pages/api/product-categories/index.js
+++ pages/api/product-categories/index.js
@@ -1,6 +1,10 @@
 import axios from "axios"
 
 export default async (req, res) => {
-    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/product-categories`)
-    return res.json(data)
+    try {
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/product-categories`)
+        return res.json(data)
+    } catch (error) {
+        return res.json(error)
+    }
 }
