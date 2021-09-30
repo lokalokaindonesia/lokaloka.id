9801701b18e1c4b4d0161b414dddd109abeb7163
Arza Aldi K <kampungvector@gmail.com>
2021-09-30 15:40:32 +0700

    casfioa

--- pages/api/expedition/cost/index.js
+++ pages/api/expedition/cost/index.js
@@ -3,22 +3,11 @@ import axios from "axios"
 export default async (req, res) => {
     if (req.method != 'POST') return res.status(405).json('METHOD NOT ALLOWED')
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_RAJA_ONGKIR_URL}/cost`, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-            'Content-Type': 'application/json',
-            key: process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY,
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify(req.body) // body data type must match "Content-Type" header
-    });
-    const data = await response.json()
-    return res.json(data.rajaongkir.results[0].costs[1].cost[0]) // parses JSON response into native JavaScript objects
-
     try {
         const { data } = await axios.post(`${process.env.NEXT_PUBLIC_RAJA_ONGKIR_URL}/cost/`, req.body, {
             headers: {
                 key: process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY,
+                "Access-Control-Allow-Origin": "https://lokaloka.id"
             },
         })
 
