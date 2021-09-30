485590dbbc97165bec02ee0fd101fb38d94db558
Arza Aldi K <kampungvector@gmail.com>
2021-09-30 16:04:01 +0700

    emboh

--- pages/api/expedition/cost/index.js
+++ pages/api/expedition/cost/index.js
@@ -4,31 +4,19 @@ import request from 'request'
 export default async (req, res) => {
     if (req.method != 'POST') return res.status(405).json('METHOD NOT ALLOWED')
 
-    var options = {
-        method: 'POST',
-        url: 'https://api.rajaongkir.com/starter/cost',
-        headers: { key: process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY, 'content-type': 'application/x-www-form-urlencoded' },
-        form: { origin: '501', destination: '114', weight: 1700, courier: 'jne' }
-    };
+    try {
+        const { data } = await axios.post(`${process.env.NEXT_PUBLIC_RAJA_ONGKIR_URL}/cost/`, req.body, {
+            headers: {
+                key: process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY,
+            },
+        })
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+        if (!data) {
+            return res.json({ value: undefined })
+        }
 
-        return res.json(body)
-    })
-    // try {
-    //     const { data } = await axios.post(`${process.env.NEXT_PUBLIC_RAJA_ONGKIR_URL}/cost/`, req.body, {
-    //         headers: {
-    //             key: process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY,
-    //         },
-    //     })
-
-    //     if (!data) {
-    //         return res.json({ value: undefined })
-    //     }
-
-    //     return res.json(data.rajaongkir.results[0].costs[1].cost[0])
-    // } catch (error) {
-    //     return res.json(error)
-    // }
+        return res.json(data.rajaongkir.results[0].costs[1].cost[0])
+    } catch (error) {
+        return res.json(error)
+    }
 }
