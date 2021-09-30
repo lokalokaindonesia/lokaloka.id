bac9e740f5ec79a834ea7a13b3a3f86ebed7d0b2
Arza Aldi K <kampungvector@gmail.com>
2021-09-30 14:14:05 +0700

    cors fak

--- pages/api/expedition/cost/index.js
+++ pages/api/expedition/cost/index.js
@@ -1,21 +1,12 @@
 import axios from "axios"
-import NextCors from "nextjs-cors";
 
 export default async (req, res) => {
+    if (req.method != 'POST') return res.status(405).json('METHOD NOT ALLOWED')
     try {
-        // Run the cors middleware
-        // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-        await NextCors(req, res, {
-            // Options
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: '*',
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        });
-
         const { data } = await axios.post(`${process.env.NEXT_PUBLIC_RAJA_ONGKIR_URL}/cost/`, req.body, {
             headers: {
                 key: process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY
-            }
+            },
         })
 
         if (!data) {
--- pages/api/expedition/provinces/index.js
+++ pages/api/expedition/provinces/index.js
@@ -1,17 +1,7 @@
 import axios from "axios"
-import NextCors from "nextjs-cors";
 
 export default async (req, res) => {
     try {
-        // Run the cors middleware
-        // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-        await NextCors(req, res, {
-            // Options
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: '*',
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        });
-
         const { data } = await axios.get(`${process.env.NEXT_PUBLIC_RAJA_ONGKIR_URL}/province/`, {
             headers: {
                 'key': process.env.NEXT_PUBLIC_RAJA_ONGKIR_API_KEY
