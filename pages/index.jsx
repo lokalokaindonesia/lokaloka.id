8bc7998842dcd9711cf2187405d6e19707d057cb
Arza Aldi K <kampungvector@gmail.com>
2021-12-27 19:36:18 +0700

    update customerly script

--- pages/index.jsx
+++ pages/index.jsx
@@ -82,28 +82,6 @@ const Home = ({ products, promo, recommended }) => {
                 {/* Just For You */}
                 <JustForYou sectionTitle='Hanya Untukmu' href='#' data={products} />
             </Layout>
-            <Script
-                strategy='lazyOnload'
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        !function(){var e=window,i=document,t="customerly",n="queue",o="load",r="settings",u=e[t]=e[t]||[];if(u.t){return void u.i("[customerly] SDK already initialized. Snippet included twice.")}u.t=!0;u.loaded=!1;u.o=["event","attribute","update","show","hide","open","close"];u[n]=[];u.i=function(t){e.console&&!u.debug&&console.error&&console.error(t)};u.u=function(e){return function(){var t=Array.prototype.slice.call(arguments);return t.unshift(e),u[n].push(t),u}};u[o]=function(t){u[r]=t||{};if(u.loaded){return void u.i("[customerly] SDK already loaded. Use customerly.update to change settings.")}u.loaded=!0;var e=i.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://messenger.customerly.io/launcher.js";var n=i.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};u.o.forEach(function(t){u[t]=u.u(t)})}();
-                        
-                        customerly.load({
-                            "app_id": "77d26414",
-                            position: {
-                                desktop: {  
-                                    bottom: 50,
-                                    side: 50
-                                },
-                                mobile: {
-                                    bottom: 60,
-                                    side: 30
-                                }
-                            }
-                        });
-                    `,
-                }}
-            />
         </Fragment>
     )
 }
