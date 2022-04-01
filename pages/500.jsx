06458be27b0513cecba13e5068cf09243be93a76
Arza Aldi K <kampungvector@gmail.com>
2022-04-01 11:00:44 +0700

    500 error page

--- pages/500.jsx
+++ pages/500.jsx
@@ -11,7 +11,7 @@ const Custom505 = () => {
                 <div className='px-4 lg:py-12'>
                     <div className='lg:gap-4 lg:flex'>
                         <div className='flex flex-col items-center justify-center md:py-24 lg:py-32'>
-                            <h1 className='font-bold text-blue-600 text-9xl'>404</h1>
+                            <h1 className='font-bold text-blue-600 text-9xl'>500</h1>
                             <p className='mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl'>
                                 <span className='text-red-500'>Oops!</span> Server Error.
                             </p>
