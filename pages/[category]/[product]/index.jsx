a2917b38c764314a023cb62fb34c3c4bb7c81062
Arza Aldi K <kampungvector@gmail.com>
2022-04-20 18:48:03 +0700

    bugfix: server error

--- pages/[category]/[product]/index.jsx
+++ pages/[category]/[product]/index.jsx
@@ -616,6 +616,7 @@ export const getServerSideProps = async ({ params }) => {
     const data = await getProduct.data
 
     const baseLink = await process.env.NEXT_URL
+
     if (!data) {
         return {
             notFound: true,
@@ -625,7 +626,7 @@ export const getServerSideProps = async ({ params }) => {
     const resSimilarProducts = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products?product_category=${data.product_category.id}&_limit=6`)
     const similarProducts = await resSimilarProducts.data
 
-    const resReviews = await axios.get(`${process.env.NEXT_URL}/api/reviews/${data.id}`)
+    const resReviews = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/reviews/${data.id}`)
     const reviews = await resReviews.data
 
     return {
