8cac9dae23e71b9cc5a656dab8eefe318dc63384
lokalokaindonesia <77106328+lokalokaindonesia@users.noreply.github.com>
2022-06-14 05:15:40 +0700

    update cover

--- components/layout/Carousel.jsx
+++ components/layout/Carousel.jsx
@@ -3,8 +3,10 @@ import { Pagination, Navigation, Autoplay } from 'swiper'
 import Image from 'next/image'
 import janMobile from '../../public/images/banner/lokalokaMagerMobile.jpg'
 import janDesktop from '../../public/images/desain-juragan-kondang/januari_lokaloka.jpg'
+import coverMobile from "../../public/images/banner/coverAndroid.jpg";
+import coverDesktop from "../../public/images/banner/coverWeb.jpg";
 import desainKondangDesktop from '../../public/images/desain-juragan-kondang/banner_pc.jpg'
-import desainkondangMobile from '../../public/images/banner/lokalokaAndroid.jpg'
+import desainkondangMobile from "../../public/images/desain-juragan-kondang/banner_mobile.jpg";
 import myDsWeb from '../../public/images/banner/valentine/ValentineWeb.png'
 import myDsMobile from '../../public/images/banner/valentine/ValentineMobile.png'
 import samaraDesktop from '../../public/images/banner/samarahotel.jpg'
@@ -20,85 +22,186 @@ import 'swiper/css/autoplay'
 const Carousel = () => {
     const itemLength = 1
     return (
-        <>
-            <div className='my-4 hidden lg:block md:my-8 px-4 md:px-12 lg:px-16 drop-shadow-lg container mx-auto'>
-                <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
-                    spaceBetween={20}
-                    autoplay
-                    loop={itemLength > 1 ? true : false}
-                    slidesPerView={1}
-                    navigation
-                    pagination={{ clickable: true }}
-                    centeredSlides={true}
-                >
-                    <SwiperSlide>
-                        <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                            <Image src={janDesktop} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={100} height={2} />
-                        </div>
-                    </SwiperSlide>
-                    {/* <SwiperSlide>
+      <>
+        <div className="my-4 hidden lg:block md:my-8 px-4 md:px-12 lg:px-16 drop-shadow-lg container mx-auto">
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            spaceBetween={20}
+            autoplay
+            loop={itemLength > 1 ? true : false}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            centeredSlides={true}
+          >
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={coverDesktop}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={2}
+                />
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={janDesktop}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={2}
+                />
+              </div>
+            </SwiperSlide>
+            {/* <SwiperSlide>
                         <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
                             <Image src={myDsWeb} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={75} height={2} />
                         </div>
                     </SwiperSlide> */}
-                     <SwiperSlide>
-                        <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                            <Image src={desainKondangDesktop} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={75} height={2} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                       <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                           <Image src={samaraDesktop} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={75} height={2} />
-                       </div>
-                   </SwiperSlide>
-                   <SwiperSlide>
-                      <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                          <Image src={zamzamDesktop} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={75} height={2} />
-                      </div>
-                  </SwiperSlide>
-                </Swiper>
-            </div>
-            <div className='my-4 lg:hidden md:my-8 px-4 md:px-12 lg:px-16 drop-shadow-md container mx-auto'>
-                <Swiper
-                    modules={[Navigation, Pagination, Autoplay]}
-                    spaceBetween={20}
-                    autoplay
-                    loop={itemLength > 1 ? true : false}
-                    slidesPerView={1}
-                    navigation
-                    pagination={{ clickable: true }}
-                    centeredSlides={true}
-                >
-                    {/* <SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={desainKondangDesktop}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={75}
+                  height={2}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={samaraDesktop}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={75}
+                  height={2}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={zamzamDesktop}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={75}
+                  height={2}
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="my-4 lg:hidden md:my-8 px-4 md:px-12 lg:px-16 drop-shadow-md container mx-auto">
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            spaceBetween={20}
+            autoplay
+            loop={itemLength > 1 ? true : false}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            centeredSlides={true}
+          >
+            {/* <SwiperSlide>
                         <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
                             <Image src={myDsMobile} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={100} height={3} />
                         </div>
                     </SwiperSlide>  */}
-                     <SwiperSlide>
-                        <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                            <Image src={desainkondangMobile} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={100} height={3} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                            <Image src={janMobile} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={100} height={3} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                            <Image src={samaraMobile} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={100} height={3} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className='w-full h-full cursor-pointer bg-slate-600 rounded-md'>
-                            <Image src={zamzamMobile} alt='Promo' className='rounded-md' layout='responsive' width={6} priority quality={100} height={3} />
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
-            </div>
-        </>
-    )
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={coverMobile}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={3}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={desainkondangMobile}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={3}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={janMobile}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={3}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={samaraMobile}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={3}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-full h-full cursor-pointer bg-slate-600 rounded-md">
+                <Image
+                  src={zamzamMobile}
+                  alt="Promo"
+                  className="rounded-md"
+                  layout="responsive"
+                  width={6}
+                  priority
+                  quality={100}
+                  height={3}
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </>
+    );
 }
 
 export default Carousel
