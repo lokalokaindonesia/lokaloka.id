2ee0727a3658cb8cfa85618e390db8042e9a74e3
lokalokaindonesia <77106328+lokalokaindonesia@users.noreply.github.com>
2022-12-31 12:53:31 +0700

    update

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default async (req, res) => {
-    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`);
+  console.log(process.env.NEXT_PUBLIC_API_URL, process.env);
+  if (!data) {
+    return res.json({ messages: 'no products' });
+  }
 
-    if (!data) {
-        return res.json({ messages: 'no products' })
-    }
-
-    return res.json(data)
-}
+  return res.json(data);
+};
